5327e1b12c9aa311cc025fae9edbed7c7b59c127
morty <alemv96@hotmail.com>
2017-07-05 16:36:22 -0400

    Inicio
    principio de proyecto

---
+++ CondominiosUCAB/CondominiosUCAB/CondominiumData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using NpgsqlTypes;
+using System.Data;
+
+namespace CondominiosUCAB
+{
+    class CondominiumData
+    {
+        public CondominiumData() { }
+
+        public string Data_Connect()
+        {
+            string serverInfo = "Server=localhost;" + "Port=5432;" + "User iD=postgres;" + "Password=1234;" + "DATABASE=ProyectoEdificios;";
+            DataSet datos = new DataSet();
+            NpgsqlConnection con = new NpgsqlConnection();
+
+
+            try
+            {
+                con.ConnectionString = serverInfo;
+                con.Open();
+                return "si se logro conexion";
+
+            }
+            catch
+            {
+              return "no se logro conexion";
+            }
+        }
+
+    }
+}
---
+++ CondominiosUCAB/CondominiosUCAB/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*using Npgsql;
+using NpgsqlTypes;
+using System.Data;*/
+
+namespace CondominiosUCAB
+{
+    static class Program{
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            CondominiumData data = new CondominiumData();
+            string message = data.Data_Connect();
+            //conexion a postgres
+        }
+    }
+}
