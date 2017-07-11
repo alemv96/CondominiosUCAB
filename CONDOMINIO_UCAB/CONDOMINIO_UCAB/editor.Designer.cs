44ce5a02b162ab4a461643057ae20db77104f508
juancarlo3007 <juancarlo30071996@gmail.com>
2017-07-10 20:23:45 -0400

    Add files via upload

--- CONDOMINIO_UCAB/CONDOMINIO_UCAB/editor.Designer.cs
+++ CONDOMINIO_UCAB/CONDOMINIO_UCAB/editor.Designer.cs
@@ -93,7 +93,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.ClientSize = new System.Drawing.Size(847, 605);
             this.Controls.Add(this.cancelar);
             this.Controls.Add(this.guardar);
